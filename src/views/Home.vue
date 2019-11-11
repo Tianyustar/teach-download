3ddeedbbb243d0ea254b5bee0dab2d1f3b060227
TianyuStar <yunxing.liu@foxmail.com>
2019-11-11 16:41:21 +0800

    nothing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <iframe
+      src="https://view.officeapps.live.com/op/view.aspx?src=http://qnimg.starliu.cn/3/土力学思考题与习题.doc"
+      width="100%"
+      height="1000px"
+      frameborder="1"
+    ></iframe>
+    <embed
+      src="http://qnimg.starliu.cn/db63d2b89b604d81ae4441f97ada2494/PKPM2010版说明书系列－JCCAD用户手册及技术条件.pdf"
+      type="application/pdf"
+      width="100%"
+      height="1000px"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  methods: {
+    download() {
+
+    }
   }
 };
 </script>
